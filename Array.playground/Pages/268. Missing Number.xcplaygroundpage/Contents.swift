9efac1a5f7b8b498b8a52a01378e1ffd64f0cd90
niafan0128@gmail.com <niafan0128@gmail.com>
2021-01-11 23:27:24 +0800

    268. Missing Number

---
+++ Array.playground/Pages/268. Missing Number.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func missingNumber(_ nums: [Int]) -> Int {
+    
+    let sortedNums = nums.sorted()
+    
+    for (index, num) in sortedNums.enumerated() {
+        if index != num {
+            return index
+        }
+    }
+    return nums.count
+}
+
+missingNumber([9,6,4,2,3,5,7,0,1])
