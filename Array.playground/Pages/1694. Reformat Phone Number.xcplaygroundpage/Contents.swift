593943a46cd8f4279598a2aecc2d2111b5123a12
niafan0128@gmail.com <niafan0128@gmail.com>
2021-01-21 23:11:59 +0800

    1694. Reformat Phone Number

---
+++ Array.playground/Pages/1694. Reformat Phone Number.xcplaygroundpage/Contents.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+// String
+func reformatNumber(_ number: String) -> String {
+    
+    let replacedNumber = number.replacingOccurrences(of: " ", with: "").replacingOccurrences(of: "-", with: "")
+
+    var formattedNumber = ""
+    
+    for (i, character) in replacedNumber.enumerated() {
+        if i % 3 == 0, i != 0 {
+            formattedNumber.append("-")
+            formattedNumber.append(character)
+        } else {
+            formattedNumber.append(character)
+        }
+    }
+    
+    // handle the 3-2-2
+    if replacedNumber.count % 3 == 1 {
+        let x = formattedNumber.removeLast()
+        formattedNumber.removeLast()
+        let y = formattedNumber.removeLast()
+        formattedNumber.append("-")
+        formattedNumber.append(y)
+        formattedNumber.append(x)
+    }
+    
+    return formattedNumber
+}
+
+reformatNumber("123 8473-342")
+reformatNumber("123 4-567")
